89dd0e372c8075dfcd2d98da8f79fac41757336e
Tanish Singh <tanishingh866@gmail.com>
2025-11-30 20:10:39 +0000

    Adding turns

--- src/Components/Grid/Grid.jsx
+++ src/Components/Grid/Grid.jsx
@@ -1,14 +1,33 @@
+import { useState } from "react";
 import Card from "../Card/Card";
 
 import './Grid.css';
 
 function Grid({ numberOfCards }){
+    const [ turn, setTurn ] = useState(false) // false -> X, true -> O
+    const [ board, setBoard ] = useState(Array(numberOfCards).fill(""));
+    function onPlay(index){
+        console.log("move turn", index);
+        if(turn == true){
+            board[index] = "O";
+        }
+        else{
+            board[index] = "X";
+        }
+        setBoard([...board]);
+        setTurn(!turn);
+    }
+
     return (
-        <div className="grid">
-            {Array(numberOfCards).fill(<Card/>).map((el, idx)=>{
-                return <Card key={idx}/>
+        <>
+            <h1 className="turn-highlighted">Current Turn : {(turn)?'O':'X'}</h1>
+            <div className="grid">
+                {board.map((value, idx)=>{
+                    return <Card onPlay={onPlay} player={value} key={idx} index={idx}/>
             })}
         </div>
+        </>
+        
     );
 }
 export default Grid;
--- src/Components/Icon/Icon.jsx
+++ src/Components/Icon/Icon.jsx
@@ -1,7 +1,8 @@
-import { FaCross, FaPen, FaRegCircle } from "react-icons/fa";
+import { FaPen, FaRegCircle } from "react-icons/fa";
+import { RxCross1 } from "react-icons/rx";
 function Icon({name}){
     if(name == "cross"){
-        return <FaCross/>
+        return <RxCross1/>
     }
     else if(name == "circle"){
         return <FaRegCircle/>
